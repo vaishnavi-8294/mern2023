9cae9992744426102c5fdfa276c1b630ef3d1148
vaishnavi-8294 <vaishnavisonikha2005@gmail.com>
2025-05-06 03:28:40 +0530

    up4

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,33 @@
 export const Home =()=>{
-    return <h1>hello this is home</h1>
+    return <>
+    <main>
+        <section className="section-hero">
+            <div className="container grid grid-two-cols" >
+                <div className="hero-content">
+                    <p>We are the world best IT company</p>
+                    <h1>Welcome to DevWith Vashu</h1>
+                    <p>
+                        Are you ready to take your business to the next level with cutting-edge
+                        IT solutions? look no further! At Vaishnavi Technical, we Specialize in providing innovative 
+                        IT services and solutions trilored to meet your unique needs,
+                    </p>
+                    <div className="btn btn-group">
+                        <a href="/contact">
+                            <button className="btn">connect Now</button>
+                        </a>
+                        <a href="/services">
+                            <button className="btn-secondry"> learn more</button>
+                        </a>
+                    </div>
+                </div>
+                {/* hero images */}
+                <div className="hero-image">
+                    <img src="/images/home.jpeg" alt="coding together" width="400" height="400" />
+                </div>
+
+            </div>
+
+        </section>
+    </main>
+    </>
 };
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,3 +1,53 @@
+import { useState } from "react";
+
 export const Login =()=>{
-  return <h1>hello this is Login</h1>
+  const [user,setUser]=useState({
+    email:"",
+    password:"",
+  });
+  const handleInput=(e)=>{
+    let name=e.target.name;
+    let value= e.target.value;
+    setUser({
+      ...user,
+      [name]:value,
+    });
+
+  };
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log(user);
+  };
+  return <>
+  <section>
+        <main>
+            <div className="section-registration">
+                <div className="container grid grid-two-cols">
+                    <div className="registration-image">
+                        <img src="/images/login.jpeg" alt="Login image"  width="500" height="500"/>
+                    </div>
+                    {/* registration Form */}
+                    <div className="registration-form">
+                        <h1 className="main-heading mb-3">Login Form</h1>
+                        <br />
+                        <form onSubmit={handleSubmit}>
+                            <div>
+                                <label htmlFor="email">email</label>
+                                <input type="email" name="email"  placeholder="Enter your email" id="email" value={user.email} onChange={handleInput} required autoComplete="off"/>
+                            </div>
+                            
+                            <div>
+                                <label htmlFor="password">password</label>
+                                <input type="password" name="password"  placeholder="password" id="password" value={user.password} onChange={handleInput}  required autoComplete="off"/>
+                            </div>
+                            <br />
+                            <button type="submit" className="btn btn-submit">Register Now</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </section>
+  
+  </>
 };
